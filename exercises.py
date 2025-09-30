7d1050696cd949b1693a74249fa9f6ec43eece43
OscarCharlie001 <obi14cool@gmail.com>
2025-09-30 12:10:04 +0100

    Third Commit

--- exercises.py
+++ exercises.py
@@ -14,16 +14,29 @@
 # Use a while loop to keep the game running until the user decides to quit
 # Use the random module to generate random numbers
 # Use if-elif-else statements to handle user input
-import random
-while True:
-    user_input = input(
-        "You wan play abi you no wan play dice? (yes/y or no/n): ").lower().strip()
-    if user_input in ['yes', 'y']:
-        dice1 = random.randint(1, 6)
-        dice2 = random.randint(1, 6)
-        print(f"You rolled a {dice1} and a {dice2}.")
-    elif user_input in ['no', 'n']:
-        print("Gerrara here mehn!! Shiit!")
-        break
-    else:
-        print("Invalid choice! Please enter 'yes', 'y', 'no', or 'n'.")
+# import random
+# while True:
+#     user_input = input(
+#         "You wan play abi you no wan play dice? (yes/y or no/n): ").lower().strip()
+#     if user_input in ['yes', 'y']:
+#         dice1 = random.randint(1, 6)
+#         dice2 = random.randint(1, 6)
+#         print(f"You rolled a {dice1} and a {dice2}.")
+#     elif user_input in ['no', 'n']:
+#         print("Gerrara here mehn!! Shiit!")
+#         break
+#     else:
+#         print("Invalid choice! Please enter 'yes', 'y', 'no', or 'n'.")
+
+# # ___________REVRSE A STRING____________________
+# user_input = input("Enter a string: ")
+# reversed_string = user_input[::-1]
+# print(f"Reversed string: {reversed_string}")
+# # ___________REVRSE A STRING____________________
+# ___________REVERSE A GROUP OF WORDS___________
+user_input = input("Enter a group of words: ")
+words = user_input.split()
+reversed_words = words[::-1]
+reversed_words = ' '.join(reversed(words))
+print(f"Reversed words: {reversed_words}")
+# ___________REVERSE A GROUP OF WORDS___________
